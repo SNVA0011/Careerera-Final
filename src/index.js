80aba2d1525138bba3e62bc895ce5585bcf34c82
diptanshu <96859907+SNVA0011@users.noreply.github.com>
2022-04-22 18:16:17 -0500

    Fixing data fetch  issue on api  & fixing console errors as much as possible
     Fixing data fetch  issue on api  & fixing console errors as much as possible

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App'
 import AppSpanish from './es/AppSpanish'
 import { BrowserRouter } from 'react-router-dom'
+ 
 
 ReactDOM.render(
     <>
