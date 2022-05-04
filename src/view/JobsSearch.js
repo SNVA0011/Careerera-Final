03788c05ca1199971dd200cef30ad97effcd48f7
diptanshu <96859907+SNVA0011@users.noreply.github.com>
2022-05-04 11:45:22 -0500

    https://careerapp.hunterwave.com/become-an-instructor
    https://careerapp.hunterwave.com/become-an-instructor

---
+++ src/view/JobsSearch.js
@@ -0,0 +1,85 @@
+import React from 'react'
+
+export default function JobsSearch() {
+    return (
+        <div className='partner-marketing-affiliates would full-w jobssearch-box'>
+            <div style={{ background: 'rgb(5 54 105 / 80%)' }} className="z-10	position-relative">
+
+                <div className="container pt-16 text-white">
+                    <div className='row'>
+                        <div className="col-xl-7 col-12 pb-4 pb-xl-0">
+                            <h1 className="text-2xl md:text-3xl font-bold leading-loose tracking-normal  text-center text-lg-left">
+                            Drop Resume & Get Your Desire Job
+                            </h1>
+
+                            <span className="text-xl md:text-2xl light font-light tracking-wide  text-center text-lg-left d-block">
+                            <i class="bi bi-search mr-2"></i>
+                            <i class="bi bi-briefcase-fill orange-clrsite"></i>
+                             Find Jobs, Employment & <strong className='orange-clrsite'>Career Opportunities</strong>
+                            </span>
+
+                            <span className="heading-border-line d-block left-0 mt-3 mb-4 mx-auto mx-lg-0 "></span>
+
+                            <h3 className='text-lg md:text-xl font-semibold tracking-normal'>With our services in your enterprise network, you can help your organisation go farther as a Channel Partner:</h3>
+                            <ul className='p-0 text-gray-300 font-medium text-base md:text-lg '>
+                                <li className='d-flex mb-2'><i className="bi bi-check2-circle mr-3 text-2xl orange-clrsite"></i> Work for India's prestigious higher education firm, which has offices in APAC, USA, Dubai, India, UK, Malaysia and Europe.</li>
+                                <li className='d-flex mb-2'><i className="bi bi-check2-circle mr-3 text-2xl orange-clrsite"></i> Develop new capabilities, innovate more quickly, and collaborate on ideas.</li>
+                                <li className='d-flex mb-2'><i className="bi bi-check2-circle mr-3 text-2xl orange-clrsite"></i> Find new strategies to expand your company's sales and pipeline</li>
+                                <li className='d-flex mb-2'><i className="bi bi-check2-circle mr-3 text-2xl orange-clrsite"></i> Become a part of a dynamic ecosystem with an ever-expanding variety of solutions.</li>
+                                <li className='d-flex mb-2'><i className="bi bi-check2-circle mr-3 text-2xl orange-clrsite"></i> Gain access to new markets and clients.</li>
+                                <li className='d-flex'><i className="bi bi-check2-circle mr-3 text-2xl orange-clrsite"></i> Demonstration of our services</li>
+                            </ul>
+
+                            <hr></hr>
+                            <div className='mt-3 text-center text-xl-left parner-uscal'>
+                                <div className='mailto-partnerjoin text-left text-base md:text-lg font-light'>
+                                    <div className="row">
+                                        <div className='col-md-6 mb-3 col-12'>
+                                            <a className="no-underline text-white d-inline-flex align-items-center font-medium tracking-wide" href="tel:+919643034607" target='_blank' rel='noreferrer' >
+                                                <i className="bi  bi-telephone roundmail-ul mr-2 orange-clrsite"></i>
+                                                <span className='d-inline-block'>+91 9643-034-607</span>
+                                            </a>
+                                        </div>
+
+                                        <div className='col-md-6 mb-3 col-12'>
+                                            <a className="no-underline text-white d-inline-flex align-items-center font-medium tracking-wide" href="https://api.whatsapp.com/send?phone=+919643034607&text=Hi%2C%20I%20am%20interested%20in%20Partner%20with%20us%20program%20of%20Careerera" target='_blank' rel='noreferrer' >
+                                                <i className="bi bi-whatsapp roundmail-ul mr-2 orange-clrsite"></i>
+                                                <span className='d-inline-block'>+91 9643-034-607</span>
+                                            </a>
+                                        </div>
+
+
+                                        <div className='col-md-6 col-12'>
+                                            <a className="no-underline text-white d-inline-flex align-items-center font-medium tracking-wide" href="mailto:partnerwithus@careerera.com" target='_blank' rel='noreferrer' >
+                                                <i className="bi bi-envelope roundmail-ul mr-2 orange-clrsite"></i>
+                                                <span className='d-inline-block'>partnerwithus@careerera.com</span>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                            </div>
+
+
+
+                        </div>
+
+
+
+                        <div className="ml-auto col-xxl-4 col-xl-5 col-12 help-page mt-4 mt-xl-0">
+
+                            DDDD
+                        </div>
+
+
+                    </div>
+ 
+
+                </div>
+
+                <div className="overflow-hidden ppt-clistaff mt-shapeneed"><div className="shape-bottom"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none"><path className="shape-fill" fill="#fff" d="M421.9,6.5c22.6-2.5,51.5,0.4,75.5,5.3c23.6,4.9,70.9,23.5,100.5,35.7c75.8,32.2,133.7,44.5,192.6,49.7  c23.6,2.1,48.7,3.5,103.4-2.5c54.7-6,106.2-25.6,106.2-25.6V0H0v30.3c0,0,72,32.6,158.4,30.5c39.2-0.7,92.8-6.7,134-22.4  c21.2-8.1,52.2-18.2,79.7-24.2C399.3,7.9,411.6,7.5,421.9,6.5z"></path></svg></div></div>
+            </div>
+        </div>
+    )
+}
