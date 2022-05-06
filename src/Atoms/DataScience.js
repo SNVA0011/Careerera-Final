53689d7995f5ab9d165cdaf52490084a0f9a694d
diptanshu <96859907+SNVA0011@users.noreply.github.com>
2022-05-06 18:15:13 -0500

    page title
    page title not showing issue fixed

--- src/Atoms/DataScience.js
+++ src/Atoms/DataScience.js
@@ -5,23 +5,24 @@ const DataScience = (props) => {
 
         return (
                 <>
-                        {final ?
-                                <div className="full-w data-scinece-othercer scroll-spbx" id="description">
-                                        <div className="container py-16">
-                                                <div className="full-w">
-                                                        <div className="sec_title">
-                                                                <h2 className="text-2xl md:text-3xl font-bold mt-0" dangerouslySetInnerHTML={{ __html: props.title }} />
-                                                                <p className="text-gray-500 text-base md:text-lg fw-medium" dangerouslySetInnerHTML={{ __html: 'Know your way around the ' + props.title + '.' }} />
-                                                                <hr className="w-20 hr mb-4 bg-blue-400 sepfoll-hr" />
-                                                        </div>
 
+                        <div className="full-w data-scinece-othercer scroll-spbx" id="description">
+                                <div className="container py-16">
+                                        <div className="full-w">
+                                                <div className="sec_title">
+                                                        <h2 className="text-2xl md:text-3xl font-bold mt-0" dangerouslySetInnerHTML={{ __html: props.title }} />
+                                                        <p className="text-gray-500 text-base md:text-lg fw-medium" dangerouslySetInnerHTML={{ __html: 'Know your way around the ' + props.title + '.' }} />
+                                                        <hr className="w-20 hr mb-4 bg-blue-400 sepfoll-hr" />
+                                                </div>
+                                                {final ?
                                                         <div className="blog-detials-single">
                                                                 <div className="content st" dangerouslySetInnerHTML={{ __html: final }} />
                                                         </div> 
-                                                </div>
+                                                : " "}
                                         </div>
                                 </div>
-                                : " "}
+                        </div>
+
                 </>
         )
 }
--- src/es/Atoms/DataScience.js
+++ src/es/Atoms/DataScience.js
@@ -6,25 +6,25 @@ const DataScience = (props) => {
 
         return (
                 <>
-                        {final ?
-                                <div className="full-w data-scinece-othercer scroll-spbx" id="description">
-                                        <div className="container py-16">
-                                                <div className="full-w">
-                                                        <div className="sec_title">
-                                                                <h2 className="text-2xl md:text-3xl font-bold mt-0" dangerouslySetInnerHTML={{ __html: props.title }} />
-                                                                <p className="text-gray-500 text-base md:text-lg fw-medium" dangerouslySetInnerHTML={{ __html: 'Conozca su camino alrededor del ' + props.title + '.' }} />
-                                                                <hr className="w-20 hr mb-4 bg-blue-400 sepfoll-hr" />
-                                                        </div>
 
+                        <div className="full-w data-scinece-othercer scroll-spbx" id="description">
+                                <div className="container py-16">
+                                        <div className="full-w">
+                                                <div className="sec_title">
+                                                        <h2 className="text-2xl md:text-3xl font-bold mt-0" dangerouslySetInnerHTML={{ __html: props.title }} />
+                                                        <p className="text-gray-500 text-base md:text-lg fw-medium" dangerouslySetInnerHTML={{ __html: 'Conozca su camino alrededor del ' + props.title + '.' }} />
+                                                        <hr className="w-20 hr mb-4 bg-blue-400 sepfoll-hr" />
+                                                </div>
+                                                {final ?
                                                         <div className="blog-detials-single">
                                                                 <div className="content st" dangerouslySetInnerHTML={{ __html: final }} />
                                                         </div>
+                                                        : " "}
 
-                                                      
-                                                </div>
                                         </div>
                                 </div>
-                                : " "}
+                        </div>
+
                 </>
         )
 }
