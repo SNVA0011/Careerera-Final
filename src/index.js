a5751cb0bff5d02f1abcae0adc2b932e141a3586
diptanshu <96859907+SNVA0011@users.noreply.github.com>
2022-05-09 17:20:45 -0500

    Issue of html showing on Curriculum section & url issue on enroll now button(&currency=Rs)
     Issue of html showing on Curriculum section & url issue on enroll now button(&currency=Rs)

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
-import React, {lazy, Suspense} from 'react'
+import React, { lazy, Suspense } from 'react'
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter } from 'react-router-dom'
 import fun from "./Images/fun4.gif"
-
 const App = lazy(() => import('./App'))
 const AppSpanish = lazy(() => import('./es/AppSpanish'))
-
 
 ReactDOM.render(
     <>
         <BrowserRouter>
-<Suspense fallback={<div><img src={fun}/></div>}>
-            {
-                window.location.pathname.split('/')[1] === 'es' ?
-                    <AppSpanish />
-                    :
-                    <App />
-            }
-</Suspense>
+            <Suspense fallback={<div><img src={fun} /></div>}>
+                {
+                    window.location.pathname.split('/')[1] === 'es' ?
+                        <AppSpanish />
+                        :
+                        <App />
+                }
+            </Suspense>
         </BrowserRouter>
     </>,
     document.getElementById('root')
